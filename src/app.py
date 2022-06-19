6af8bcf503e8bd751ab46fb0beaee78bc116d927
dataheiress <estherobi98@gmail.com>
2022-06-19 01:58:28 +0100

    Completed a Python Function that checks if a number is Prime or not

--- src/app.py
+++ src/app.py
@@ -4,6 +4,18 @@ import os
 
 def prime(s):
     # your code goes here
+    input = int(s)
+
+    if input <= 1:
+        return False
+
+    for i in range(2, input):
+        if input % i == 0:
+            return False
+    
+    return True
+    
+
 
 def solution(s):
     return prime(s)
